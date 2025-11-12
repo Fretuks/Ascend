8a33adb201ca6419b76cf791fdffe00fd3c32cc9
KrisHD1337 <kriscerni@gmail.com>
2025-11-12 16:11:16 +0100

    Adjusted files to work with neoforge

--- src/main/java/net/fretux/ascend/event/PlayerEvents.java
+++ src/main/java/net/fretux/ascend/event/PlayerEvents.java
@@ -6,32 +6,20 @@ import net.fretux.ascend.player.StatEffects;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 
-@Mod.EventBusSubscriber(modid = AscendMod.MODID)
+
+@EventBusSubscriber(modid = AscendMod.MODID)
 public class PlayerEvents {
-
-    private static final ResourceLocation PLAYER_STATS_ID =
-            new ResourceLocation(AscendMod.MODID, "player_stats");
-
-    @SubscribeEvent
-    public static void onAttachCapabilities(AttachCapabilitiesEvent<Entity> event) {
-        if (event.getObject() instanceof Player player) {
-            event.addCapability(PLAYER_STATS_ID, new PlayerStatsProvider());
-        }
-    }
 
     @SubscribeEvent
     public static void onPlayerClone(PlayerEvent.Clone event) {
-        event.getOriginal().reviveCaps();
-        event.getOriginal().getCapability(PlayerStatsProvider.PLAYER_STATS).ifPresent(oldStats ->
-                event.getEntity().getCapability(PlayerStatsProvider.PLAYER_STATS).ifPresent(newStats -> {
+        var oldStats = event.getOriginal().getData(PlayerStatsProvider.PLAYER_STATS);
+                var newStats = event.getEntity().getData(PlayerStatsProvider.PLAYER_STATS);
                     newStats.deserializeNBT(oldStats.serializeNBT());
-                })
-        );
+
         if (!event.getEntity().level().isClientSide) {
             StatEffects.applyAll(event.getEntity());
             PlayerStatsProvider.sync(event.getEntity());
