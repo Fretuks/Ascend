a46d95ad2b85819abe2db62697ca9db83856ffe7
KrisHD1337 <kriscerni@gmail.com>
2025-11-12 11:13:29 +0100

    Adjusted files to work with neoforge

---
+++ src/main/java/net/fretux/ascend/network/ModNetworking.java
@@ -0,0 +1,29 @@
+package net.fretux.ascend.network;
+
+import net.fretux.ascend.AscendMod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+
+@EventBusSubscriber(modid = AscendMod.MODID, bus =  EventBusSubscriber.Bus.MOD)
+public class ModNetworking {
+
+    @SubscribeEvent
+    public static void register(final RegisterPayloadHandlersEvent event) {
+        final PayloadRegistrar registrar = event.registrar("1.0");
+
+        registrar.playToServer(
+                ServerStatsPayload.TYPE,
+                ServerStatsPayload.CODEC,
+                ServerStatsPayload::handleServer
+        );
+
+        registrar.playToClient(
+                ClientStatsPayload.TYPE,
+                ClientStatsPayload.CODEC,
+                ClientStatsPayload::handleClient
+        );
+    }
+}
+
